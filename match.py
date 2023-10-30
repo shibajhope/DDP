9010a5e4dab8d9d72010ff60af202cdbe833a114
shibajhope <146071875+shibajhope@users.noreply.github.com>
2023-10-30 08:11:41 +0700

    Add files via upload
    Signed-off-by: shibajhope <146071875+shibajhope@users.noreply.github.com>

---
+++ match.py
@@ -0,0 +1,9 @@
+luasBangunDatar = int (input("pilih angka sesuai yang kalian butuhkan"))
+
+match luasBangunDatar :
+    case 1 :
+        print("Menghitung luas persegi")
+    case 2 :
+        print("Menghitung luas lingkaran")
+    case 3 :
+        print("Menghitung luas segitiga")
