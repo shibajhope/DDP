234b7a9fb0b0e1f192186fd3f5ade729682c7318
shibajhope <146071875+shibajhope@users.noreply.github.com>
2023-10-31 12:58:58 +0700

    Add files via upload
    Signed-off-by: shibajhope <146071875+shibajhope@users.noreply.github.com>

--- match.py
+++ match.py
@@ -1,9 +1,28 @@
-luasBangunDatar = int (input("pilih angka sesuai yang kalian butuhkan"))
+ket = '''selamat datang di aplikasi menghitung
+masukan pilihan:
+1. untuk menghitung luas persegi
+2. untuk menghitung luas lingkaran
+3. untuk menghitung luas segitiga
+'''
 
-match luasBangunDatar :
-    case 1 :
-        print("Menghitung luas persegi")
-    case 2 :
-        print("Menghitung luas lingkaran")
-    case 3 :
-        print("Menghitung luas segitiga")
+pilihan = input (ket)
+
+match pilihan:
+    case "1":
+        print ("kamu memilih 1 untuk menghitung luas persegi")
+        sisi = int(input ("masukan sisi ?"))
+        luasP = sisi * sisi
+        print ("luas persegi yang sisinya ", sisi, "adalah " ,luasP)
+    case "2":
+        print ("kamu memilih 2 untuk menghitung luas lingkaran")
+        jari2 = float(input ("masukan jari2 ?"))
+        luasL = 3.14 * jari2 * jari2
+        print ("luas lingkaran yang jari2nya ", jari2, "adalah ",int(luasL))
+    case "3":
+        print ("kamu memilih 2 untuk menghitung luas segitiga")
+        alas = int(input ("masukan alas ?"))
+        tinggi = int(input ("masukan tinggi ?"))
+        luasS = 0,5 * alas * tinggi
+        print ("luas segitiga yang alasnya ", alas, "dan tingginya ", tinggi, "adalah", int(luasS))
+    case _:
+        print("salah memilih pilihan")
