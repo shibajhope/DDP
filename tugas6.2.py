474aacc52ecde9e99d8ce382d370cf5437f024f9
shibajhope <146071875+shibajhope@users.noreply.github.com>
2023-11-03 11:07:07 +0700

    Add files via upload
    Signed-off-by: shibajhope <146071875+shibajhope@users.noreply.github.com>

---
+++ tugas6.2.py
@@ -0,0 +1,4 @@
+total = 0
+for i in range (1,20,2):
+    total = total + i
+print("1 + 3 + 5 + 7 + 9 +11 + 13 + 15 + 15 + 17 +19 =" ,total)
---
+++ tugas6.3.py
@@ -0,0 +1,3 @@
+baris = int(input('masukan baris:'))
+for i in range(1,baris + 1):
+    print(i * "*")
