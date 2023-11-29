33f31d632d6985d38beb4eb54cdb7ae26c502afe
shibajhope <146071875+shibajhope@users.noreply.github.com>
2023-11-29 11:33:15 +0700

    Delete DDP directory
    Signed-off-by: shibajhope <146071875+shibajhope@users.noreply.github.com>

--- DDP/only_consonant.py
+++
@@ -1,9 +0,0 @@
-def konsonan(kalimat):
-    vocal = "aiueo "
-    hasil = ""
-    for huruf in kalimat:
-        if huruf not in vocal:
-            hasil += huruf
-    return hasil
-
-print(konsonan("Nurul Fikri"))
